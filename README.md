2e454df09fd4baf65371ae262297f0cf823ca2ec
dmitryuv <uvarov.dl@gmail.com>
2023-03-21 19:04:46 +0300

    initial commit
    few changes and improvements (see readme), the new code is a bit messy and was a quick fix for my needs

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # synology-surveillance-api-motion-telegram-gifs
 A python script to create animated gifs from videos recorded by cameras attached to Synology Surveillance Station and send them via telegram bot API
+
+Forked from [original project](https://github.com/dmitryuv/synology-surveillance-api-motion-telegram-gifs) with few key changes and small fixes:
+1. Updated API version, tested only on DSM 7+ with Surveillance Station 9+.
+2. Added support for per-camera telegram targets, including different bot tokens per my needs.
+3. Added support for CMS configuration, where cameras are on different recording servers.
+4. Improved video fetching logic - given 10sec polling interval, it ensures that video have at least skip+length seconds if still recording to avoid too short and truncated results.
+
+Supports multiple cameras polling and ffmpeg parameters
+Remembers already processed events across restarts.
+
+## Config File
+
+Needs a simple JSON based config file passed in on the command line.
+
+For example:
+
+```json
+{
+  "data_folder": "./data", // <-- Leave it empty if you are using docker image
+  "ffmpeg_working_folder": "./gifs",
+  "tele_bot_token": "XXX", // <-- default token for all cameras, can be empty if specified per camera
+  "tele_chat_id": 123, // <-- default chat id for all cameras, can be empty if specified per camera
+  "synology_base_api_url": "http://127.0.0.1",
+  "synology_user": "admin",
+  "synology_password": "password123",
+  "synology_cameras": [
+    {
+      "id": 1, 
+      "skip_first_n_secs": 5, //<-- Skip seconds recorded before motion event is triggered
+      "max_length_secs": 5, //<-- Do not create gif for video full length but only with first n seconds
+      "scale": 320, //<-- Determine quality and size of the output gif
+      "tele_bot_token": "XXX", //<-- custom bot token for this camera
+      "tele_chat_id": 123 //<-- custom chat id for this camera
+    },
+    {
+      "id": 2,
+      "skip_first_n_secs": 7,
+      "max_length_secs": 10,
+      "scale": 640,
+      "topic_name": "camera_2"
+    }
+  ]
+}
+
+```
+* `data_folder`: Path where to stored sqlite db for already processed events (preserve state across restarts). Leave empty if using docker image.
+* `tele_bot_token`: Telegram Bot token, this is a default global setting, can be skipped when using per-camera setting
+* `tele_chat_id`: Target chat id, this is a default global setting, can be skipped when using per-camera setting
+* `ffmpeg_working_folder`: Working folder for downloaded mp4 videos and created GIFs
+* `synology_base_api_url`: Base url of Synology Surveillance Station APIs
+* `synology_user`: User to access Synology Surveillance Station APIs
+* `synology_password`: User's password to access Synology Surveillance Station APIs
+* `synology_cameras`: Array of cameras for events polling
+    * `id`: Synology Surveillance Station camera id
+    * `skip_first_n_secs`: Skip seconds recorded before motion event is triggered
+    * `max_length_secs`: Do not create gif for video full length but only with first n seconds
+    * `scale`: Determine quality and size of the output gif
+    * `tele_bot_token`: Per-camera Telegram settings
+    * `tele_chat_id`: Per-camera Telgram settings
+
+If you don't know camera ids, leave cameras section empty and you'll get ids printed at first run
+```
+"synology_cameras": []
+```
+Example:
+```
+[INFO] (MainThread) Synology Info Camera Id 1 Name arzilla_veranda IP 192.168.1.87
+[INFO] (MainThread) Synology Info Camera Id 2 Name arzilla_piazzale IP 192.168.1.88
+[INFO] (MainThread) Synology Info Camera Id 3 Name arzilla_campo IP 192.168.1.148
+[INFO] (MainThread) Synology Info Camera Id 4 Name arzilla_veranda_interno IP 192.168.1.126
+```
+
+## Installation
+
+You need to build a docker image first if you want to run it in a container.
+
+If you'd prefer to install dependencies yourself, you'll need:
+
+* ffmpeg 4.0 (other versions probably work, but that's what I tested with)
+* Python 3.9
+* python libraries listed in `requirements.txt` (install via `pip install -r requirements.txt`)
--- syno_telegram_gifs.py
+++ syno_telegram_gifs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import datetime
 import json
 import logging
@@ -7,23 +7,32 @@ import sqlite3
 import subprocess
 import sys
 import time
-import telegram
+import urllib.parse
+import telebot
+from pathlib import Path
 from sqlite3 import Error
-import requests
 
-logging.basicConfig(level=logging.DEBUG,
+import requests
+# disable SSL exceptions
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] [%(levelname)s] (%(threadName)-10s) %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 # SYNO.API urls
-synoApiLoginUrl = "{}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=2" \
-                  "&session=SurveillanceStation&format=sid&account={}&passwd={}"
+logged_in = False
+synoApiLoginUrl = "{}/webapi/auth.cgi?api=SYNO.API.Auth&method=login&version=6" \
+                  "&session=SurveillanceStation&format=cookie&account={}&passwd={}"
 synoApiCamerasInfoUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Camera&method=List&version=1&_sid={}"
-synoApiEventQueryUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Event&method=List" \
-                       "&version=4&locked=0&reason=2&limit=1&cameraIds={}&_sid={}"
-synoApiEventDownloadUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Event" \
-                          "&method=Download&version=4&analyevent=false&mountId=0" \
-                          "&eventId={}&_sid={}"
+synoApiEventQueryUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Recording&method=List" \
+                       "&version=5&locked=0&reason=2&limit=1&cameraIds={}&evtSrcType={}&evtSrcId={}&_sid={}"
+synoApiEventDownloadUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Recording" \
+                          "&method=Download&version=6&id={}&_sid={}"
+synoApiCMSEventDownloadUrl = "{}/webapi/entry.cgi?api=SYNO.SurveillanceStation.CMS&method=Redirect&version=1&dsId={}&webAPI={}&isDownloadFile=true&_sid={}"
+synoApiCMSRedirectArgs = "{{\"api\":\"SYNO.SurveillanceStation.Recording\",\"version\":6,\"method\":\"Download\",\"id\":{}}}"
 
 sql_create_processed_events_table = """ CREATE TABLE IF NOT EXISTS processed_events (
                                         id integer PRIMARY KEY,
@@ -69,7 +78,7 @@ def check_already_processed_event_by_camera(conn, camera_id, event_id):
 
     already_processed = False
     for row in rows:
-        logging.error("Event %s already processed %s", event_id, row)
+        #logging.error("Event %s already processed %s", event_id, row)
         already_processed = True
 
     return already_processed
@@ -92,7 +101,7 @@ def syno_login(base_url, user, password):
     if login_response.ok:
         login_data = json.loads(login_response.content.decode('utf-8'))
         if login_data["success"]:
-            logging.info('login_response got sinotoken %s', login_data["data"]["sid"])
+            #logging.info('login_response got sinotoken %s', login_data["data"]["sid"])
             return login_data["data"]["sid"]
         else:
             return ""
@@ -103,7 +112,7 @@ def syno_login(base_url, user, password):
 
 def syno_info(base_url, sid):
     info_response = requests.get(synoApiCamerasInfoUrl.format(base_url, sid), verify=False)
-    logging.info('info_response status_code %s', info_response.status_code)
+    #logging.info('info_response status_code %s', info_response.status_code)
 
     if info_response.ok:
         info_data = json.loads(info_response.content.decode('utf-8'))
@@ -113,51 +122,50 @@ def syno_info(base_url, sid):
         info_response.raise_for_status()
 
 
-def syno_last_event(base_url, camera_id, sid):
-    event_response = requests.get(synoApiEventQueryUrl.format(base_url, camera_id, sid),
+def syno_last_event(base_url, camera_id, camera_time, srcType, srcId, sid):
+    event_response = requests.get(synoApiEventQueryUrl.format(base_url, camera_id, srcType, srcId, sid),
                                   verify=False)
-    logging.info('event_response status_code %s', event_response.status_code)
+    #logging.info('event_response status_code %s', event_response.status_code)
 
     if event_response.ok:
         event_data = json.loads(event_response.content.decode('utf-8'))
-        if len(event_data["data"]["events"]) > 0 and event_data["data"]["events"][0]["cameraId"] == camera_id:
-            logging.info('found event for camera %s', event_data["data"]["events"][0]["camera_name"])
+        
+        if not event_data["success"]:
+            err_code = event_data["error"]["code"]
+            # handle auth failure and exit to re-authenticate
+            if (code >= 105 and code <= 107) or code == 119:
+                logged_in = False
+            return -1, 0
 
-            epoch_start = event_data["data"]["events"][0]["startTime"]
-            epoch_stop = event_data["data"]["events"][0]["stopTime"]
-            utc_start = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_start))
-            utc_stop = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_stop))
-            diff_seconds = epoch_stop - epoch_start
-            diff_mins = ((diff_seconds // 60) % 60)
-            diff_hours = ((diff_mins // 60) % 60)
+        if len(event_data["data"]["events"]) == 0:
+            return -1, 0
 
-            video_length = "{:02d}:{:02d}:{:02d}".format(diff_hours, diff_mins, diff_seconds)
-            syno_event_data = {'event_id': event_data["data"]["events"][0]["eventId"],
-                               'camera_id': event_data["data"]["events"][0]["cameraId"],
-                               'camera_name': event_data["data"]["events"][0]["camera_name"],
-                               'gif': '',
-                               'epoch_start_time': epoch_start,
-                               'epoch_stop_time': epoch_stop,
-                               'utc_start_time': utc_start,
-                               'utc_stop_time': utc_stop,
-                               'video_length': video_length}
+        event_rec = event_data["data"]["events"][0]
+        rec_time = event_rec["stopTime"] - event_rec["startTime"]
 
-            return syno_event_data
-        else:
-            return -1
-
+        if event_rec["cameraId"] == camera_id and (event_rec["recording"] == False or (event_rec["recording"] == True and rec_time >= camera_time)):
+            #logging.info('found event for camera %s', event_rec["camera_name"])
+            return event_rec["id"], event_rec["dsId"]
     else:
         event_response.raise_for_status()
+    
+    return -1, 0
 
 
-def syno_download_video(download_dir, base_url, event_id, sid):
+def syno_download_video(download_dir, base_url, event_id, event_ds_id, sid):
     outfile_gif = '{}/{}.mp4'.format(download_dir, event_id)
 
     with open(outfile_gif, "wb") as f:
         logging.info('Downloading video for event id %i to %s .....', event_id, outfile_gif)
-        download_response = requests.get(synoApiEventDownloadUrl.format(base_url, event_id, sid),
-                                         verify=False, stream=True)
-        logging.info('download_response status_code %s', download_response.status_code)
+        
+        if event_ds_id > 0:
+            redirectArgs = synoApiCMSRedirectArgs.format(event_id)
+            uri = synoApiCMSEventDownloadUrl.format(base_url, event_ds_id, redirectArgs, sid)
+        else:
+            uri = synoApiEventDownloadUrl.format(base_url, event_id, sid)
+
+        download_response = requests.get(uri, verify=False, stream=True)
+        #logging.info('download_response status_code %s', download_response.status_code)
 
         if download_response.ok:
             total_length = download_response.headers.get('content-length')
@@ -171,7 +179,7 @@ def syno_download_video(download_dir, base_url, event_id, sid):
                     dl += len(data)
                     f.write(data)
                     done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                    #sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
                     sys.stdout.flush()
             logging.info('Downloading video for event id %i to %s .....DONE', event_id, outfile_gif)
             return outfile_gif
@@ -186,7 +194,7 @@ def convert_video_gif(scale, skip_first_n_secs, max_length_secs, input_video, ou
 
     retcode = subprocess.call([
         "ffmpeg", "-stats", "-i", input_video, "-vf",
-        "fps=15,scale={}:-1:flags=lanczos".format(scale), "-an",
+        "fps=15,scale={}:-1:flags=lanczos".format(scale),
         "-ss", "00:00:" + "{}".format(skip_first_n_secs).zfill(2), "-t", "{}".format(max_length_secs), "-y",
         str(output_gif)
     ])
@@ -200,59 +208,62 @@ class CameraMotionEventHandler:
         self.camera = camera
         self.config = config
         self.sid = sid
-        self.bot = telegram.Bot(self.config["telegram_bot_token"])
-        # Keep history of events processed so we can guard against duplicate
         self.processed_events_conn = processed_events_conn
 
-    def publish_telegram_document(self, event_data):
-        logging.info('publish_telegram_document %s %s', event_data['gif'], self.camera["id"])
-
-        retcode = False
+    def publish_telegram_message(self, gif):
         try:
-            self.bot.send_document(chat_id=self.config["chat_id"],
-                                   document=open(event_data['gif'], 'rb'),
-                                   caption='Video from camera {}. {}-{}, duration {}'.format(event_data['camera_name'],
-                                                                                             event_data[
-                                                                                                 'utc_start_time'],
-                                                                                             event_data[
-                                                                                                 'utc_stop_time'],
-                                                                                             event_data[
-                                                                                                 'video_length']))
-            retcode = True
+            if not "bot" in self.camera:
+                logging.error("Camera %s does not have bot configured", self.camera["id"])
+                return True
+
+            tb = self.camera["bot"]
+            chat_id = self.camera["tele_chat_id"]
+            tb.send_chat_action(chat_id, 'upload_video')
+
+            fb = open(gif, "rb")
+            retcode = tb.send_animation(chat_id, fb, disable_notification=True)
+            fb.close()
+
+            # remove file on success
+            os.remove(gif)
+
+            return True
         except Error as e:
-            logging.error("CANNOT SEND DOCUMENT", e)
-        return retcode
+            logging.error("Telegram exception", e)
+
+            return False
+
+
 
     def poll_event(self):
-        logging.info('Start getting last camera event for camera %s %s', self.camera["id"], self.camera["name"])
-        event_data = syno_last_event(self.base_url, self.camera["id"], self.sid)
-        event_id = event_data['event_id']
+        #logging.info('Start getting last camera event for camera %s', self.camera["id"])
+        camera_time = self.camera["skip_first_n_secs"] + self.camera["max_length_secs"]
+        camera_id = self.camera["id"]
+        event_id, event_ds_id = syno_last_event(self.base_url, camera_id, camera_time, self.camera["srcType"], self.camera["dsId"], self.sid)
         if event_id > -1:
-            if check_already_processed_event_by_camera(self.processed_events_conn, self.camera["id"], event_id):
-                logging.info('Event %s already processed', event_id)
+            if check_already_processed_event_by_camera(self.processed_events_conn, camera_id, event_id):
+                #logging.info('Event %s already processed', event_id)
                 return None, None
 
-            logging.info('Start downloading event video for event_id %s', event_id)
-            mp4_file = syno_download_video(self.config["ffmpeg_working_folder"], self.base_url, event_id, self.sid)
+            logging.info('Start downloading event video for event_id %i, camera_id %i', event_id, camera_id)
+            mp4_file = syno_download_video(self.config["ffmpeg_working_folder"], self.base_url, event_id, event_ds_id, self.sid)
             outfile_gif = '{}/{}.gif'.format(self.config["ffmpeg_working_folder"], event_id)
             convert_retcode = convert_video_gif(self.camera["scale"],
                                                 self.camera["skip_first_n_secs"],
                                                 self.camera["max_length_secs"],
                                                 mp4_file, outfile_gif)
             if convert_retcode == 0:
-                event_data['gif'] = outfile_gif
-                public_retcode = self.publish_telegram_document(event_data)
-                if public_retcode:
-                    processed_event = (self.camera["id"], event_id, datetime.datetime.now());
+                tele_retcode = self.publish_telegram_message(outfile_gif)
+                if tele_retcode:
+                    processed_event = (camera_id, event_id, datetime.datetime.now());
                     replace_processed_events(self.processed_events_conn, processed_event)
-                    logging.info('Done processing event_id %i', event_id)
+                    logging.info('Done processing event_id  %i, camera_id %i', event_id, camera_id)
                 else:
-                    logging.error('Invalid return code from mqtt publish for event id %i camera name %s', event_id,
-                                  self.camera["name"])
+                    logging.error('Invalid return code from telegram send for event_id %i, camera_id %i', event_id, camera_id)
             else:
                 logging.error('Invalid return code from ffmpeg subprocess call for event id %i', event_id)
-        else:
-            logging.info('No event found for camera %s %s', self.camera["id"], self.camera["name"])
+        #else:
+            #logging.info('No event found for camera %s', self.camera["id"])
 
 
 def main():
@@ -287,18 +298,43 @@ def main():
                     continue
                 else:
                     logged_in = True
-                    logging.info('Synology Auth ok %s', sid)
+                    logging.info('Synology Auth ok')
                     info_data = syno_info(config["synology_base_api_url"], sid)
                     for camera_info in info_data["data"]["cameras"]:
-                        logging.info('Synology Info Camera Id %s Name %s IP %s', camera_info["id"], camera_info["name"],
-                                     camera_info["host"])
+                        logging.warning('Synology Info Camera Id %s Name %s IP %s DsID %s', camera_info["id"], camera_info["name"],
+                                     camera_info["host"], camera_info["ownerDsId"])
+
+                        # TODO: that's a bit messy additional setup, needs to be improved later
+                        for camera in config["synology_cameras"]:
+                            if camera["id"] == camera_info["id"]:
+                                camera["dsId"] = camera_info["ownerDsId"]
+                                # delete active handler during re-auth
+                                camera.pop("handler", None)
+
+                                if int(camera["dsId"]) > 0:
+                                    camera["srcType"] = 2  # type 2 - recording server
+                                else:
+                                    camera["srcType"] = 0  # type 0 - host server
+
+                                if "tele_bot_token" in camera:
+                                    camera["bot"] = telebot.TeleBot(camera["tele_bot_token"])
+                                elif "tele_bot_token" in config:
+                                    if not "bot" in config:
+                                        config["bot"] = telebot.TeleBot(config["tele_bot_token"])
+                                    camera["bot"] = config["bot"]
+
+                                if not "tele_chat_id" in camera and "tele_chat_id" in config:
+                                    camera["tele_chat_id"] = config["tele_chat_id"]
+                                break
 
             for camera in config["synology_cameras"]:
-                logging.info('CameraMotionEventHandler  poll_event %s %s', camera["id"], camera["name"])
-                camera_handler = CameraMotionEventHandler(processed_events_conn, config["synology_base_api_url"],
+                #logging.info('CameraMotionEventHandler poll_event %s', camera["id"])
+                if not "handler" in camera:
+                    logging.info('CameraMotionEventHandler created poll_event %s', camera["id"])
+                    camera["handler"] = CameraMotionEventHandler(processed_events_conn, config["synology_base_api_url"],
                                                           camera,
                                                           config, sid)
-                camera_handler.poll_event()
+                camera["handler"].poll_event()
 
     except KeyboardInterrupt:
         logging.info('KeyboardInterrupt')
